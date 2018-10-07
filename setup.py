7e3f1ec6d75229e3d6f41d54c66d8d5b83791ab7
Wenping Cui <wenpingcui@gmail.com>
2018-10-07 12:44:53 -0400

    Changes to be committed: 	modified:   setup.py add package dependence

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
-setup(name='Ecofunctions',
-      version='0.1',
-      description='Simulations about consumer resource models',
-      url='http://github.com/storborg/funniest',
-      author='Wenping Cui',
-      author_email='wenpingcui@gmail.com',
-      license='MIT',
-      packages=['Eco_function'],
-      zip_safe=False)
+setup(name='Eco_function',
+    version='0.1',
+    description='Simulations about consumer resource models',
+    url='https://github.com/Wenping-Cui/Eco_functions',
+    author='Wenping Cui',
+    author_email='wenpingcui@gmail.com',
+    license='MIT',
+    packages=['Eco_function'],
+    install_requires=['numpy','scipy','cvxopt','pandas','matplotlib'],
+    zip_safe=False)
